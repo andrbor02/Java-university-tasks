1395f8d360376304d58f21e4926dd92ed8b9a388
andrbor <andrbor02@yandex.ru>
2021-09-29 18:05:26 +0300

    29.09.21 Упражнения [1-3]

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,41 @@
-import javax.swing.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        ///Exc3 exc3 = new Exc3(11, 2);
-        ///Exc4 exc4 = new Exc4(3, 15); // я немного изменил условие задачи: программа находит первые 15 чисел, удовлетворяющих данному условию. Если я нахожу числа по условию программы возникает StackOverFlowError
-        ///Exc5 exc5 = new Exc5(-345);
-        ///Exc6 exc6 = new Exc6(199); // не log(n)
-        ///Exc7 exc7 = new Exc7(149);
-        ///Exc8 exc8 = new Exc8("AROZAYPALANALAPYAZORA");
+        Student[] students = new Student[10];
+        students[0] = new Student(1, "Vasya", 280);
+        students[1] = new Student(3, "Kolya", 240);
+        students[2] = new Student(5, "Archi", 258);
+        students[3] = new Student(8, "Vanya", 232);
+        students[4] = new Student(2, "Seva", 259);
+        students[5] = new Student(6, "Van", 252);
+        students[6] = new Student(7, "Billy", 212);
+        students[7] = new Student(10, "Ilya", 245);
+        students[8] = new Student(9, "Dima", 283);
+        students[9] = new Student(4, "Timur", 228);
+
+// 1
+//        LinkedList<Student> sts1 = StudentSort.sortByIDNumber(students);
+//        Iterator<Student> it = sts1.descendingIterator();
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+
+// 2
+//        SortingStudentsByGPA sts2 = new SortingStudentsByGPA();
+//        sts2.quickSort(students, 0, students.length - 1);
+//        for (Student student : students) {
+//            System.out.println(student.toString());
+//        }
+
+// 3
+        SortingStudentsMergeSort sts3 = new SortingStudentsMergeSort();
+        sts3.SortUnsorted(students, 0, students.length - 1);
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
+
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,32 @@
+public class Student {
+    private int number;
+    private String name;
+    private int finalBall;
+
+    public Student(int number, String name, int finalBall) {
+        this.number = number;
+        this.name = name;
+        this.finalBall = finalBall;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFinalBall() {
+        return finalBall;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "number=" + number +
+                ", name='" + name + '\'' +
+                ", finalBall=" + finalBall +
+                '}';
+    }
+}
